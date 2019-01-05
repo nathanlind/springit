61264079e89a74f6ba0290803904a4319f985fe9
Nathan Lindsey <nathan.nwa@gmail.com>
2019-01-05 17:47:59 -0600

    Adding mappings from link->comment

--- src/main/java/com/nathanlind/springit/model/Comment.java
+++ src/main/java/com/nathanlind/springit/model/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @NoArgsConstructor
@@ -17,6 +18,6 @@ public class Comment {
     private Long id;
     private String body;
 
-    // Link
-
+    @ManyToOne
+    private Link link;
 }
