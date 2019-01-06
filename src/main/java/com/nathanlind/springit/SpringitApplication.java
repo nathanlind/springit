8769828d8f0433ed1ba11e0cc489e8c1f8f26d17
Nathan Lindsey <nathan.nwa@gmail.com>
2019-01-05 18:01:29 -0600

    Adding abstract class Auditable and extending it in Link & Comment

--- src/main/java/com/nathanlind/springit/SpringitApplication.java
+++ src/main/java/com/nathanlind/springit/SpringitApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringitApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
     }
