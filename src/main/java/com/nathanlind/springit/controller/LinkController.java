7f43a0dd682a94737c6e12f0ef6093a9a67b7cce
Nathan Lindsey <nathan.nwa@gmail.com>
2019-01-06 20:48:08 -0600

    Sets up HomeController and LinkController

---
+++ src/main/java/com/nathanlind/springit/controller/LinkController.java
@@ -0,0 +1,48 @@
+package com.nathanlind.springit.controller;
+
+import com.nathanlind.springit.model.Link;
+import com.nathanlind.springit.repository.LinkRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/links")
+public class LinkController {
+
+    private LinkRepository linkRepository;
+
+    public LinkController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    // list
+    @GetMapping("/")
+    public List<Link> list() {
+        return linkRepository.findAll();
+    }
+
+    // CRUD
+    @PostMapping("/create")
+    public Link create(@ModelAttribute Link link) {
+        return linkRepository.save(link);
+    }
+
+    // /links/1
+    @GetMapping("/{id}")
+    public Optional<Link> read(@PathVariable Long id) {
+        return linkRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Link update(@PathVariable Long id, @ModelAttribute Link link) {
+        // get the id
+        return linkRepository.save(link);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        linkRepository.deleteById(id);
+    }
+}
