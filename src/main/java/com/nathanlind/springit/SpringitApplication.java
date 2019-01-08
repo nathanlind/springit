deb7ac1716f9c2022bfa7f72836957b4e2e54c15
Nathan Lindsey <nathan.nwa@gmail.com>
2019-01-08 14:00:40 -0600

    Builds out view layer

--- src/main/java/com/nathanlind/springit/SpringitApplication.java
+++ src/main/java/com/nathanlind/springit/SpringitApplication.java
@@ -1,12 +1,9 @@
 package com.nathanlind.springit;
 
-import com.nathanlind.springit.model.Comment;
-import com.nathanlind.springit.model.Link;
-import com.nathanlind.springit.repository.CommentRepository;
-import com.nathanlind.springit.repository.LinkRepository;
-import org.springframework.boot.CommandLineRunner;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -18,19 +15,9 @@ public class SpringitApplication {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
-    //@Bean
-    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
-        return args -> {
-            Link link = new Link("Getting Started with Spring Boot 2", "https://therealdanvega.com/spring-boot-2-docs");
-            linkRepository.save(link);
-
-            Comment comment = new Comment("This Spring Boot 2 link is awesome!", link);
-            commentRepository.save(comment);
-            link.addComment(comment);
-
-            System.out.println("We just inserted a link and a comment");
-            System.out.println("===========================================================");
-        };
+    @Bean
+    PrettyTime prettyTime() {
+        return new PrettyTime();
     }
 }
 
